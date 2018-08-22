00a5248fab657e8ced653e73ecd9bbd02edd3474
LlamaDad <40605479+LlamaDad@users.noreply.github.com>
2018-08-22 15:47:46 -0400

    Add Credential to Windows Cred Manager
    This tool is created to add an encrypted password and username to the WIndows Credential Manager

---
+++ AzureConnect.ps1
@@ -0,0 +1,7 @@
+﻿#Connection Script to Azure
+#Which Tenant is this?
+$TenantID = "Id Here"
+
+#Get User Credentials
+$cred = .\Tools\credman -GetCred -Target "PrdTgt"
+connect-msolservice -credential $cred
